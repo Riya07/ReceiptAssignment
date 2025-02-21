809f65fa150de58c0861a1eb79c78fb5ea43d88b
Riya Aghara <riya.aghara@gmail.com>
2025-02-21 13:21:36 -0800

    Update receipt_test.go

--- receipts/receipt_test.go
+++ receipts/receipt_test.go
@@ -4,44 +4,6 @@ import (
 	"testing"
 )
 
-// func Test_processReceipt(t *testing.T) {
-// 	type args struct {
-// 		w http.ResponseWriter
-// 		r *http.Request
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			processReceipt(tt.args.w, tt.args.r)
-// 		})
-// 	}
-// }
-
-// func Test_calculatePoints(t *testing.T) {
-// 	type args struct {
-// 		receipt Receipt
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want int
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := calculatePoints(tt.args.receipt); got != tt.want {
-// 				t.Errorf("calculatePoints() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
-
 // Test Points Calculation Logic
 func TestCalculatePoints(t *testing.T) {
 	testReceipt := Receipt{
